2ae1781e32e5491a78a1b7ec01559d6aa0fb5862
Kenneth Reitz <me@kennethreitz.com>
2010-07-13 08:55:03 -0400

    runner formating

--- tabbed.py
+++ tabbed.py
@@ -1,21 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# _____         ______  ______        _________
-# __  /_______ ____  /_ ___  /_ _____ ______  /
-# _  __/_  __ `/__  __ \__  __ \_  _ \_  __  / 
-# / /_  / /_/ / _  /_/ /_  /_/ //  __// /_/ /  
-# \__/  \__,_/  /_.___/ /_.___/ \___/ \__,_/
-
-
 """
 Tabbed                                            
 Copyright (c) 2010 Kenneth Reitz. MIT License.
 """
 
 import tabbed.cli
-import tabbed
-
 
 
 if __name__ == '__main__':
