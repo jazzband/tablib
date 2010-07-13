00dfe719d1550891428e80de13f0979996f31ae9
Kenneth Reitz <me@kennethreitz.com>
2010-07-13 09:30:14 -0400

    ultra simple cli interface

---
+++ tabbed/cli.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+
+from helpers import *
+# from core import Parse
+
+from packages import opster
+
+
+opts = (
+	('v', 'version', False, 'Report tabbed version'),
+	('', 'to', False, 'Output format')
+)
+
+
+@opster.command(options=opts, usage='[FILE] [--to] [FILE]')
+def start(**opts):
+	""" Converts dataset formats """
+	
+	print opts
+	
+	opts = Object(**opts
