b869f83d8740de55a1ae66592d082ac98bb7b0b9
Kenneth Reitz <me@kennethreitz.com>
2010-08-29 17:36:52 -0400

    Version bump

--- tablib/core.py
+++ tablib/core.py
@@ -6,8 +6,8 @@
 # / /_  / /_/ / _  /_/ /_  /_/ //  __// /_/ /  
 # \__/  \__,_/  /_.___/ /_.___/ \___/ \__,_/   
 
-__version__ = '0.0.2'
-__build__ = '0x000002'
+__version__ = '0.0.3'
+__build__ = '0x000003'
 __author__ = 'Kenneth Reitz'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2010 Kenneth Reitz'
