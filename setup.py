275ac9d194fb2fdc90634961e77ad72247c25a66
Kenneth Reitz <me@kennethreitz.com>
2010-09-12 14:12:30 -0400

    Setup.py typo

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ if sys.argv[-1] == "publish":
 setup(
 	name='tablib',
 	version='0.6.1',
-	description='Format agnostic tabular data library (XLS, CSV, JSON, YAML, CSV)',
+	description='Format agnostic tabular data library (XLS, JSON, YAML, CSV)',
 	long_description=open('README.rst').read() + '\n\n' +
 	                 open('HISTORY.rst').read(),
 	author='Kenneth Reitz',
