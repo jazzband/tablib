35102ab951f0231f817e3c57be1741ce8580625d
Kenneth Reitz <me@kennethreitz.com>
2010-09-11 23:08:57 -0400

    Prepping for distribution.

--- setup.py
+++ setup.py
@@ -18,26 +18,27 @@ if sys.argv[-1] == "publish":
 
 setup(
 	name='tablib',
-	version='0.0.4',
-	description='Python wrapper for Gist API',
+	version='0.6.0',
+	description='Format agnostic tabular data library (XLS, CSV, JSON, YAML, CSV)',
 	long_description=open('README.rst').read() + '\n\n' +
 	                 open('HISTORY.rst').read(),
 	author='Kenneth Reitz',
 	author_email='me@kennethreitz.com',
 	url='http://github.com/kennethreitz/tablib',
 	packages=['tablib'],
+	install_requires=['xlwt', 'simplejson', 'PyYAML'],
 	license='MIT',
 	classifiers=(
 		'Development Status :: 4 - Beta',
 		'License :: OSI Approved :: MIT License',
 		'Programming Language :: Python',
-		'Programming Language :: Python :: 2.5',
-		'Programming Language :: Python :: 2.6',
+        # 'Programming Language :: Python :: 2.5',
+        # 'Programming Language :: Python :: 2.6',
 		'Programming Language :: Python :: 2.7',
 	),
-	entry_points={
-		'console_scripts': [
-			'tabbed = tablib.cli:start',
-		],
-	}
+    # entry_points={
+    #   'console_scripts': [
+    #       'tabbed = tablib.cli:start',
+    #   ],
+    # }
 )
