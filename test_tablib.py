a7471f7302ab6fba0740198c203d9af4efe8c837
Kenneth Reitz <me@kennethreitz.com>
2010-09-13 17:21:40 -0400

    Testing fixtures for fixed bugs.

--- test_tablib.py
+++ test_tablib.py
@@ -13,12 +13,27 @@ class TablibTestCase(unittest.TestCase):
 		pass
 
 	def test_empty_append(self):
+
 		data = tablib.Dataset()
+
 		new_row = (1,2,3)
 		data.append(new_row)
-		assert data.width == len(new_row)
+
+		self.assertTrue(data.width == len(new_row))
+
+
+	def test_empty_append_with_headers(self):
+
+		data = tablib.Dataset()
+
+		data.headers = ['first', 'second']
+		new_row = (1,2,3,4)
 		
-	
+		self.assertRaises(tablib.InvalidDimensions, data.append, new_row)
+		
+	# def test_adding_header with (self):
+				
+
 
 
 if __name__ == '__main__':
