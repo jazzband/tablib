93c6c3958172875354a688d430e92f0d95428e6a
Luke Lee <durdenmisc@gmail.com>
2010-09-13 20:23:31 -0500

    Misc. pep8 cleanups including spaces after ',' and blank line organization

--- test_tablib.py
+++ test_tablib.py
@@ -5,6 +5,7 @@ import unittest
 
 import tablib
 
+
 class TablibTestCase(unittest.TestCase):
 	def setUp(self):
 		pass
@@ -16,24 +17,21 @@ class TablibTestCase(unittest.TestCase):
 
 		data = tablib.Dataset()
 
-		new_row = (1,2,3)
+		new_row = (1, 2, 3)
 		data.append(new_row)
 
 		self.assertTrue(data.width == len(new_row))
-
 
 	def test_empty_append_with_headers(self):
 
 		data = tablib.Dataset()
 
 		data.headers = ['first', 'second']
-		new_row = (1,2,3,4)
-		
-		self.assertRaises(tablib.InvalidDimensions, data.append, new_row)
-		
-	# def test_adding_header with (self):
-				
+		new_row = (1, 2, 3, 4)
 
+		self.assertRaises(tablib.InvalidDimensions, data.append, new_row)
+
+	# def test_adding_header with (self):
 
 
 if __name__ == '__main__':
