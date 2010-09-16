dbcea81c170b0bb101a23428e648034d1ab41258
Kenneth Reitz <me@kennethreitz.com>
2010-09-16 00:59:58 -0400

    Inline docs.

--- tablib/helpers.py
+++ tablib/helpers.py
@@ -16,5 +16,6 @@ class Struct(object):
 def piped():
 	"""Returns piped input via stdin, else False."""
 	with sys.stdin as stdin:
+		# TTY is only way to detect if stdin contains data
 		return stdin.read() if not stdin.isatty() else None
 
