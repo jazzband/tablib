7364995eaabf49d35652754158151a232af08a22
Kenneth Reitz <me@kennethreitz.com>
2010-09-20 21:39:27 -0400

    Version bump (v0.7.1)

--- tablib/core.py
+++ tablib/core.py
@@ -21,8 +21,8 @@ from helpers import *
 # __all__ = ['Dataset', 'DataBook']
 
 __name__ = 'tablib'
-__version__ = '0.7.0'
-__build__ = 0x000700
+__version__ = '0.7.1'
+__build__ = 0x000701
 __author__ = 'Kenneth Reitz'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2010 Kenneth Reitz'
