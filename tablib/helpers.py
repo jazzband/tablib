c3fa29a166bc91c202eee6faa432283b14fc46f3
Kenneth Reitz <me@kennethreitz.com>
2010-09-25 04:51:56 -0400

    Added public method for pylint.

--- tablib/helpers.py
+++ tablib/helpers.py
@@ -15,6 +15,10 @@ class Struct(object):
 	def __getitem__(self, key):
 		return getattr(self, key, None)
 
+	def dictionary(self):
+		"""Returns dictionary representation of object."""
+		return self.__dict__
+
 
 def piped():
 	"""Returns piped input via stdin, else False."""
