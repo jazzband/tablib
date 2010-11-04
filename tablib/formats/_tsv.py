2b529bcb1c630c173a03a2d2bc22c05cb7ce81f7
Kenneth Reitz <me@kennethreitz.com>
2010-11-04 01:06:07 -0400

    Quotation constancy.

--- tablib/formats/_tsv.py
+++ tablib/formats/_tsv.py
@@ -18,7 +18,7 @@ extentions = ('tsv',)
 def export_set(dataset):
 	"""Returns a TSV representation of Dataset."""
 	stream = cStringIO.StringIO()
-	_tsv = csv.writer(stream, delimiter="\t")
+	_tsv = csv.writer(stream, delimiter='\t')
 
 	for row in dataset._package(dicts=False):
 		_tsv.writerow(row)
@@ -28,12 +28,10 @@ def export_set(dataset):
 
 def import_set(dset, in_stream, headers=True):
 	"""Returns dataset from TSV stream."""
-
 	dset.wipe()
 
-	rows = csv.reader(in_stream.split("\r\n"), delimiter="\t")
+	rows = csv.reader(in_stream.split('\r\n'), delimiter='\t')
 	for i, row in enumerate(rows):
-
 		# Skip empty rows
 		if not row:
 			continue
@@ -47,7 +45,7 @@ def import_set(dset, in_stream, headers=True):
 def detect(stream):
 	"""Returns True if given stream is valid TSV."""
 	try:
-		rows = dialect = csv.Sniffer().sniff(stream, delimiters="\t")
+		rows = dialect = csv.Sniffer().sniff(stream, delimiters='\t')
 		return True
 	except csv.Error:
 		return False
