25da44f56980ac8ddc7aa4b034ab0ec9a1ebd5e7
Luca Beltrame <einar@heavensinferno.net>
2010-11-21 13:00:56 +0100

    Support for HTML (export only). Unit-tested. Depends on the "markup.py" package(http://markup.sourceforge.net) which is included in packages/ Notice that the tests now depend on the presence of markup.py.

---
+++ tablib/formats/_html.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+""" Tablib - HTML export support.
+"""
+
+import cStringIO
+
+import markup
+import tablib
+
+title = 'html'
+extentions = ('html', )
+
+def export_set(dataset):
+
+	"""HTML representation of a Dataset."""
+
+	stream = cStringIO.StringIO()
+
+	page = markup.page()
+	page.table.open()
+
+	if dataset.headers is not None:
+		page.thead.open()
+		headers = markup.oneliner.th(dataset.headers)
+		page.tr(headers)
+		page.thead.close()
+
+	for row in dataset:
+		html_row = markup.oneliner.td(row)
+		page.tr(html_row)
+
+	page.table.close()
+
+	stream.writelines(str(page))
+
+	return stream.getvalue()
+
