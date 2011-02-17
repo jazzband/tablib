ccb29c68fa814424f9fc761fc6ab3d24779a8cc6
Kenneth Reitz <me@kennethreitz.com>
2011-02-17 16:31:52 -0500

    *shutter* making everyone else happy

--- setup.py
+++ setup.py
@@ -8,43 +8,43 @@ from distutils.core import setup
 
 
 def publish():
-	"""Publish to PyPi"""
-	os.system("python setup.py sdist upload")
+    """Publish to PyPi"""
+    os.system("python setup.py sdist upload")
 
 if sys.argv[-1] == "publish":
-	publish()
-	sys.exit()
+    publish()
+    sys.exit()
 
 required = []
 
 setup(
-	name='tablib',
-	version='0.9.3',
-	description='Format agnostic tabular data library (XLS, JSON, YAML, CSV)',
-	long_description=open('README.rst').read() + '\n\n' +
-	                 open('HISTORY.rst').read(),
-	author='Kenneth Reitz',
-	author_email='me@kennethreitz.com',
-	url='http://tablib.org',
-	packages= [
-		'tablib', 'tablib.formats', 
-		'tablib.packages',
-		'tablib.packages.simplejson',
-		'tablib.packages.xlwt',
-		'tablib.packages.yaml', 
-	],
-	install_requires=required,
-	license='MIT',
-	classifiers=(
-		'Development Status :: 5 - Production/Stable',
-		'Intended Audience :: Developers',
-		'Natural Language :: English',
-		'License :: OSI Approved :: MIT License',
-		'Programming Language :: Python',
+    name='tablib',
+    version='0.9.3',
+    description='Format agnostic tabular data library (XLS, JSON, YAML, CSV)',
+    long_description=open('README.rst').read() + '\n\n' +
+                     open('HISTORY.rst').read(),
+    author='Kenneth Reitz',
+    author_email='me@kennethreitz.com',
+    url='http://tablib.org',
+    packages= [
+        'tablib', 'tablib.formats', 
+        'tablib.packages',
+        'tablib.packages.simplejson',
+        'tablib.packages.xlwt',
+        'tablib.packages.yaml', 
+    ],
+    install_requires=required,
+    license='MIT',
+    classifiers=(
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
         # 'Programming Language :: Python :: 2.5',
         'Programming Language :: Python :: 2.6',
-		'Programming Language :: Python :: 2.7',
-		# 'Programming Language :: Python :: 3.0',
-		# 'Programming Language :: Python :: 3.1',
-	),
+        'Programming Language :: Python :: 2.7',
+        # 'Programming Language :: Python :: 3.0',
+        # 'Programming Language :: Python :: 3.1',
+    ),
 )
