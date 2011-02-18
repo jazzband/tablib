5cac9bd97e519a44b75217a82051f63a57a42a4c
Kenneth Reitz <me@kennethreitz.com>
2011-02-18 02:42:57 -0500

    Python 2.5 added to compatible language list

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ setup(
         'Natural Language :: English',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
-        # 'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.5',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         # 'Programming Language :: Python :: 3.0',
