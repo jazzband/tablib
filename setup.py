1b97b7191ecf84f0fd0eb0ae7e8d49146c89f894
Kenneth Reitz <me@kennethreitz.com>
2011-03-23 04:24:25 -0400

    pre-version bump

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ if sys.version_info[:2] < (2,6):
 
 setup(
     name='tablib',
-    version='0.9.4',
+    version='0.9.5',
     description='Format agnostic tabular data library (XLS, JSON, YAML, CSV)',
     long_description=open('README.rst').read() + '\n\n' +
                      open('HISTORY.rst').read(),
