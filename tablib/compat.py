999c49a4f092422f8340849dbd72399233149a86
Kenneth Reitz <me@kennethreitz.com>
2011-05-11 19:01:35 -0400

    initial compat module

---
+++ tablib/compat.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+"""
+tablib.compat
+~~~~~~~~~~~~~
+
+Tablib compatiblity module.
+
+"""
+
+import sys
+
+
+from tablib.core25 import (
+    Databook, Dataset, detect, import_set,
+    InvalidDatasetType, InvalidDimensions, UnsupportedFormat
+    )
+
