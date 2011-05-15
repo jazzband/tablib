f6fa3f2abc9f7328d28fb211019a1e5cd18c77f0
Kenneth Reitz <me@kennethreitz.com>
2011-05-15 13:28:10 -0400

    change (c) attribution

--- docs/conf.py
+++ docs/conf.py
@@ -41,7 +41,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Tablib'
-copyright = u'2011, Kenneth Reitz. Styles (modified) &copy; Armin Ronacher'
+copyright = u'2011, Kenneth Reitz.'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
