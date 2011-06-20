83b71967b9ca3b00d28bed38c7a6e1c3bd7c04af
Kenneth Reitz <me@kennethreitz.com>
2011-06-20 12:55:43 -0400

    integrate omnijson

--- tablib/formats/_json.py
+++ tablib/formats/_json.py
@@ -6,7 +6,7 @@
 import tablib
 
 import sys
-from tablib.compat import anyjson
+from tablib.packages import omnijson as json
 
 
 title = 'json'
@@ -15,26 +15,26 @@ extentions = ('json', 'jsn')
 
 def export_set(dataset):
     """Returns JSON representation of Dataset."""
-    return anyjson.serialize(dataset.dict)
+    return json.dumps(dataset.dict)
 
 
 def export_book(databook):
     """Returns JSON representation of Databook."""
-    return anyjson.serialize(databook._package())
+    return json.dumps(databook._package())
 
 
 def import_set(dset, in_stream):
     """Returns dataset from JSON stream."""
 
     dset.wipe()
-    dset.dict = anyjson.deserialize(in_stream)
+    dset.dict = json.dumps(in_stream)
 
 
 def import_book(dbook, in_stream):
     """Returns databook from JSON stream."""
 
     dbook.wipe()
-    for sheet in anyjson.deserialize(in_stream):
+    for sheet in json.loads(in_stream):
         data = tablib.Dataset()
         data.title = sheet['title']
         data.dict = sheet['data']
@@ -44,7 +44,7 @@ def import_book(dbook, in_stream):
 def detect(stream):
     """Returns True if given stream is valid JSON."""
     try:
-        anyjson.deserialize(stream)
+        json.loads(stream)
         return True
     except ValueError:
         return False
