f01d65c2e9d288743302acc2b494af0a7c24ce09
Kenneth Reitz <me@kennethreitz.com>
2011-08-15 22:45:35 -0400

    I don't remember merging that..

--- tablib/formats/_xls.py
+++ tablib/formats/_xls.py
@@ -5,7 +5,7 @@
 
 import sys
 
-from tablib.compat import BytesIO, xlwt, xlrd
+from tablib.compat import BytesIO, xlwt
 import tablib
 
 title = 'xls'
@@ -15,37 +15,6 @@ extentions = ('xls',)
 wrap = xlwt.easyxf("alignment: wrap on")
 bold = xlwt.easyxf("font: bold on")
 
-
-def import_set(dset, in_stream, headers=True):
-    """Returns dataset from XLS stream."""
-
-    dset.wipe()
-    
-    wb = xlrd.open_workbook(file_contents=in_stream)
-    ws = wb.sheet_by_index(0)
-    
-    for i in range(ws.nrows):
-        if (i == 0) and (headers):
-            dset.headers = ws.row_values(i)
-        else:
-            dset.append(ws.row_values(i))
-
-            
-def import_book(dbook, in_stream, headers=True):
-    """Returns databook from XLS stream."""
-
-    dbook.wipe()
-    
-    wb = xlrd.open_workbook(file_contents=in_stream)
-    for ws in wb.sheets():
-        data = tablib.Dataset()
-        data.title = ws.name
-        for i in range(ws.nrows):
-            if (i == 0) and (headers):
-                data.headers = ws.row_values(i)
-            else:
-                data.append(ws.row_values(i))
-        dbook.add_sheet(data)
 
 
 def export_set(dataset):
@@ -110,13 +79,3 @@ def dset_sheet(dataset, ws):
                         ws.write(i, j, col)
                 except TypeError:
                     ws.write(i, j, col)
-
-
-def detect(stream):
-    """Returns True if given stream is valid XLS."""
-    
-    try:
-        xlrd.open_workbook(file_contents=stream)
-        return True
-    except xlrd.XLRDError:
-        return False
