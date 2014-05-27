3da155ce0d8b5d5c2e149eeea6d56c47379e5f93
Kenneth Reitz <me@kennethreitz.com>
2014-05-27 12:49:11 -0400

    3.3, 3.4

--- setup.py
+++ setup.py
@@ -80,6 +80,8 @@ setup(
         'Programming Language :: Python :: 3.0',
         'Programming Language :: Python :: 3.1',
         'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
     ),
     tests_require=['pytest'],
 )
