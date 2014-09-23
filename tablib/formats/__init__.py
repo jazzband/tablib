0a194fff8b759d0b0f7af153bc81b1b06afe967d
Alex Rattray <rattray.alex@gmail.com>
2014-09-23 14:58:54 +0800

    prefer tsv to xlsx in formats

--- tablib/formats/__init__.py
+++ tablib/formats/__init__.py
@@ -13,4 +13,4 @@ from . import _xlsx as xlsx
 from . import _ods as ods
 from . import _dbf as dbf
 
-available = (json, xls, yaml, csv, xlsx, dbf, tsv, html, ods)
+available = (json, xls, yaml, csv, tsv, xlsx, dbf, html, ods)
