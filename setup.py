5b94682df3c0815a820f6d50b2573259d2f623c2
Hugo <hugovk@users.noreply.github.com>
2019-10-18 20:46:23 +0300

    Add support for Python 3.8

--- setup.py
+++ setup.py
@@ -47,6 +47,7 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
     python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*',
     install_requires=install,
