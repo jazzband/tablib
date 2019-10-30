5ab0507a7a9c8eac5ed45a06243e1fb21477f96a
Daniel <dfsantosbu@unal.edu.co>
2019-10-30 13:49:06 +0100

    Implement missing class method to export cli.

--- src/tablib/formats/_cli.py
+++ src/tablib/formats/_cli.py
@@ -9,8 +9,14 @@ DEFAULT_FORMAT = 'plain'
 
 def export_set(dataset, **kwargs):
     """Returns CLI representation of Dataset."""
-    if( dataset.headers is not None ): 
+    if( dataset.headers is not None ):
         kwargs.setdefault('headers', dataset.headers)
     kwargs.setdefault('tablefmt', DEFAULT_FORMAT)
     return tabulate( dataset, **kwargs)
 
+def export_book(dataset, **kwargs):
+    """Returns CLI representation of Dataset."""
+    if( dataset.headers is not None ):
+        kwargs.setdefault('headers', dataset.headers)
+    kwargs.setdefault('tablefmt', DEFAULT_FORMAT)
+    return tabulate( dataset, **kwargs)
