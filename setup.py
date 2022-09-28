4a6860fcb04e714264337721ee5ce4ee721be163
Hugo van Kemenade <hugovk@users.noreply.github.com>
2022-09-28 13:02:12 +0300

    Add support for Python 3.11

--- setup.py
+++ setup.py
@@ -37,6 +37,7 @@ setup(
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
     ],
     python_requires='>=3.7',
     extras_require={
